a11313058f465eaff900606b6df6dc37b80ddf8f
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 14:11:58 +0200

    terzo commitù

--- src/main/java/com/example/MyThread.java
+++ src/main/java/com/example/MyThread.java
@@ -26,8 +26,11 @@ public class MyThread extends Thread {
             int tentativi = 0;
             int num = random.nextInt(100);
             int numeroRicevuto;
+            String scelta;
+            boolean fine = false;
             do {
                 try {
+                    tentativi++;
                     numeroRicevuto = Integer.parseInt(in.readLine());
                     if(numeroRicevuto >= 100 || numeroRicevuto < 0){
                         risposta = "!";
@@ -37,16 +40,42 @@ public class MyThread extends Thread {
                         risposta = ">";
                     }else{
                         risposta = "=";
+                        fine = true;
                     }
+                    System.out.println(num);
                     System.out.println("Numero ricevuto: " + numeroRicevuto);
                 } catch (Exception e) {
                     System.out.println("Valore ricevuto inaccettabile");
                     risposta = "!";
                 }
-                tentativi++;
                 out.writeBytes(risposta + "\n");
-            } while (!(risposta.equals("=")));
-            out.writeBytes(tentativi + "\n");
+                if(fine){
+                    out.writeBytes(tentativi + "\n");
+                    boolean controllo = false;
+                    do{
+                        controllo = false;
+                        scelta = in.readLine();
+                        switch (scelta) {
+                            case "1":
+                                System.out.println(scelta);
+                                out.writeBytes("1\n");
+                                num = random.nextInt(100);
+                                fine = false;
+                                break;
+                            case "0":
+                            System.out.println(scelta);
+                                out.writeBytes("0\n");
+                                break;
+                            default:
+                            System.out.println(scelta);
+                                out.writeBytes("!\n");
+                                controllo = true;
+                                break;
+                        }
+                    }while(controllo);
+                }
+            } while (!fine);
+            
             s.close();
         } catch (IOException e) {
             risposta = "!";
